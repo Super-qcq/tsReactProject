094530766c295feffbae32be5306490526eb906e
qcq <980412699@qq.com>
2024-06-27 20:00:02 +0800

    4

--- src/about/component/DeliveryTaskList.tsx
+++ src/about/component/DeliveryTaskList.tsx
@@ -1,22 +1,23 @@
 import * as React from 'react'
-import "../css/task.less"
-import { Button, Modal } from 'antd';
+import '../css/task.less'
+import { Button, Modal } from 'antd'
 
-interface DeliveryTaskListProps {
-}
+interface DeliveryTaskListProps { }
 interface DeliveryTaskListStates {
     isModal: boolean
 }
 
 /**
  * 上传任务列表
- * 
- * @author 
+ *
+ * @author
  */
-export class DeliveryTaskList extends React.Component<DeliveryTaskListProps, DeliveryTaskListStates> {
-
+export class DeliveryTaskList extends React.Component<
+    DeliveryTaskListProps,
+    DeliveryTaskListStates
+> {
     constructor(props: DeliveryTaskListProps) {
-        super(props);
+        super(props)
         this.state = {
             isModal: false
         }
@@ -25,33 +26,37 @@ export class DeliveryTaskList extends React.Component<DeliveryTaskListProps, Del
     private isModalOpen = (isModal: boolean) => {
         let i = 1
         let arr = []
-        arr.forEach(element => {
-
-        })
+        arr.forEach(element => { })
         while (i < 10) {
             i++
         }
 
         this.setState({
-            isModal
+            isModal,
         })
     }
 
-    componentDidMount(): void {
-
-    }
+    componentDidMount(): void { }
 
     render() {
         const { isModal } = this.state
-        return <div className='header'>
-            <Button type="primary" onClick={() => this.isModalOpen(true)}>Button</Button>
-            <a href='http://localhost:8080/home'>dsadasda</a>
-            <Modal title="Basic Modal" open={isModal} onCancel={() => this.isModalOpen(false)}>
-                <p>wawww其实dsaSome contents...</p>
-                <p>Some contentdawdas...</p>
-                <p>wwwqqqqqqqqqqqqqqqqqqqqqqqqqqqqSome contents...</p>
-            </Modal>
-        </div >
-    } t
+        return (
+            <div className='header'>
+                <Button type='primary' onClick={() => this.isModalOpen(true)}>
+                    Button
+                </Button>
+                <a href='http://localhost:8080/home'>dsadasda</a>
+                <Modal
+                    title='Basic Modal'
+                    open={isModal}
+                    onCancel={() => this.isModalOpen(false)}
+                >
+                    <p>wawww其实dsaSome contents...</p>
+                    <p>Some contentdawdas...</p>
+                    <p>wwwqqqqqqqqqqqqqqqqqqqqqqqqqqqqSome contents...</p>
+                </Modal>
+            </div>
+        )
+    }
+    t
 }
-
